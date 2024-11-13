c52d06df8c057ddebb5e1ae6e5b3ce8cd1e8cbe2
Byron Eli Cantillano <33431345+bcantillano@users.noreply.github.com>
2024-11-12 20:27:04 -0500

    Data persistence + Data Retrieval into searchable list
    Added core data functionality to save response from sample url, saving to db, and pulling data to show in Menu view in list form

--- Little Lemon/Little Lemon/Menu.swift
+++ Little Lemon/Little Lemon/Menu.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct Menu: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State var searchText = ""
+    
     var body: some View {
         VStack{
             Text("Little Lemon Restaurant");
             Text("Chicago");
             Text("Your local restaurant serving fresh Mediterranean food!");
-            List{}
+            
+            TextField("Search menu", text: $searchText)
+                                    .textFieldStyle(.roundedBorder)
+            
+            FetchedObjects(predicate: buildPredicate(), sortDescriptors: buildSortDescriptors()) {
+                                (dishes: [Dish]) in
+                                List(dishes) { dish in
+                                    Text("Item: \(dish.title!) - Price: \(dish.price!)")
+                                }
+                                .listStyle(.plain)
+                            }
+            
+        }.onAppear(){
+            getMenuData(viewContext: viewContext);
         }
     }
+    
+    func buildSortDescriptors() -> [NSSortDescriptor] {
+            return [NSSortDescriptor(key: "title",
+                                      ascending: true,
+                                      selector:
+                                        #selector(NSString.localizedStandardCompare))]
+        }
+
+    func buildPredicate() -> NSPredicate {
+        let search = searchText == "" ? NSPredicate(value: true) : NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+        
+        return search
+        }
 }
 
 #Preview {
